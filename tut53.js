98d556fb226a3b67952a5b2468884a1e33a6dab8
Aniket-2023iem <aniketmukherjee768@gmail.com>
2023-08-26 23:46:16 +0530

    Initial commit

---
+++ tut53.js
@@ -0,0 +1,17 @@
+console.log("hello world")
+function greet(name, text="gandu"){
+    console.log(name + " " + text);
+} 
+function add(a, b , c){
+    let d = a + b + c;
+    return d;
+}
+let text1 = "ki re banchod"
+let name = "harry";
+let name1 = "sagar";
+greet(name, text1);
+greet(name1);
+let returnval = add(1,1,2);
+console.log(returnval)
+//console.log(name + " is a good boy ");
+//console.log(name1);
